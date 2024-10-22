4f72bde9f7b530e8a486fdea2e74dd1e0d252297
HalfToothed <abhishekandmails@gmail.com>
2024-10-23 01:09:02 +0530

    update_task

--- todo/todo.go
+++ todo/todo.go
@@ -28,8 +28,12 @@ func main() {
 			List()
 
 		} else if input == 3 {
-			Delete()
+			Update()
+
 		} else if input == 4 {
+			Delete()
+
+		} else if input == 5 {
 			fmt.Print("Exiting the application. Goodbye!")
 			break
 		}
@@ -39,8 +43,9 @@ func main() {
 func Options() {
 	fmt.Print("1. Add a new task \n")
 	fmt.Print("2. View all the tasks \n")
-	fmt.Print("3. Delete a task \n")
-	fmt.Print("4. Exit \n\n")
+	fmt.Print("3. Update a task \n")
+	fmt.Print("4. Delete a task \n")
+	fmt.Print("5. Exit \n\n")
 }
 
 func Add() {
@@ -63,6 +68,28 @@ func List() {
 	for index, value := range data {
 		fmt.Printf("%d. %s", index+1, value)
 	}
+}
+
+func Update() {
+	var index int
+
+	fmt.Print("Write the index of task you want to update\n")
+	fmt.Scanln(&index)
+
+	if index > 0 && len(data) >= index {
+		fmt.Print("Write the Task: \n")
+
+		reader := bufio.NewReader(os.Stdin)
+
+		updatedTask, _ := reader.ReadString('\n')
+
+		data[index-1] = updatedTask
+
+		fmt.Print("Task updated successfully \n")
+	} else {
+		fmt.Print("Index out of range \n")
+	}
+
 }
 
 func Delete() {
