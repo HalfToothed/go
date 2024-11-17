007caf252896d68347b869afb57b3f8a053bb723
HalfToothed <abhishekandmails@gmail.com>
2024-11-17 17:09:54 +0530

    query and body in httpserver

--- httpserver/main.go
+++ httpserver/main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 )
@@ -15,15 +16,33 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 
 	ctx := r.Context()
 
-	fmt.Printf("%s: got / request\n", ctx.Value(keyServerAddr))
+	hasFirst := r.URL.Query().Has("first")
+	first := r.URL.Query().Get("first")
+	hasSecond := r.URL.Query().Has("second")
+	second := r.URL.Query().Get("second")
+
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Printf("could not read body: %s", err)
+	}
+
+	fmt.Printf("%s: got / request. first(%t)=%s, second(%t)=%s\n body=%s\n", ctx.Value(keyServerAddr), hasFirst, first, hasSecond, second, body)
 	io.WriteString(w, "This is a website\n")
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
 
 	ctx := r.Context()
+
+	myName := r.PostFormValue("myName")
+
+	if myName == "" {
+		myName = "HTTP"
+	}
+
 	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
-	io.WriteString(w, "Hello, HTTP!\n")
+	io.WriteString(w, fmt.Sprintf("Hello %s!", myName))
 }
 
 func main() {
