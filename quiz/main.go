1258742fe4968096e1b1162c271fbf335ab1b728
HalfToothed <abhishekandmails@gmail.com>
2024-12-14 21:39:07 +0530

    quiz timer

--- quiz/main.go
+++ quiz/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
+	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
 type Problem struct {
@@ -12,10 +16,12 @@ type Problem struct {
 
 func main() {
 
+	timeLimit := flag.Int("limit", 10, "the time limit for quiz in seconds")
+	flag.Parse()
+
 	var problems []Problem
 
 	file, err := os.Open("problems.csv")
-
 	if err != nil {
 		panic(err)
 	}
@@ -23,9 +29,7 @@ func main() {
 	defer file.Close()
 
 	reader := csv.NewReader(file)
-
 	reader.FieldsPerRecord = -1
-
 	data, err := reader.ReadAll()
 
 	if err != nil {
@@ -36,9 +40,36 @@ func main() {
 
 		pro := Problem{
 			question: row[0],
-			answer:   row[1],
+			answer:   strings.TrimSpace(row[1]),
 		}
 
 		problems = append(problems, pro)
 	}
+
+	correct := 0
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	answerCh := make(chan string)
+
+problemLoop:
+	for i := 0; i < len(problems); i++ {
+		fmt.Printf("give an answer of %s : ", problems[i].question)
+
+		go func() {
+			var answer string
+			fmt.Scan(&answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println("\nTime's Up")
+			break problemLoop
+		case answer := <-answerCh:
+			if problems[i].answer == answer {
+				correct++
+			}
+		}
+	}
+
+	fmt.Printf("You answered %d correctly out of %d problems \n", correct, len(problems))
 }
