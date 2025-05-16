b4fc3c0bf7cb16c879bb253c488a51fd62547d25
HalfToothed <abhishekandmails@gmail.com>
2025-05-16 07:49:15 +0530

    link

---
+++ effective_go/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+
+	a = 1
+
+	fmt.Println(a)
+
+	b := `abc`
+	fmt.Println(b)
+
+	c := `\n\n`
+	fmt.Println(c)
+
+	d := "\n"
+	fmt.Println(d)
+}
